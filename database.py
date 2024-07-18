cf990e17c32904cdc10fb160511ec27eadd2cc2c
vitortoniolo <133904035+vitortoniolo@users.noreply.github.com>
2024-07-18 10:55:19 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configuração do banco de dados
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
@@ -10,7 +9,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Função para obter a sessão do banco de dados
 def get_db():
     db = SessionLocal()
     try:
@@ -18,7 +16,6 @@ def get_db():
     finally:
         db.close()
 
-# Modelo do banco de dados para o produto
 class Product(Base):
     __tablename__ = "products"
 
